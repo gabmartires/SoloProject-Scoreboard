9625a0f63f7ed368b12677a549fe59fd5e826871
Gabriel Martires <gmartire@montevallo.edu>
2024-03-07 00:58:40 -0600

    Adding Points Function Refactoring

--- index.js
+++ index.js
@@ -10,33 +10,14 @@ let guestScore = document.getElementById("guest-scoreEl");
 let home = 0;
 let guest = 0;
 
-function addHome1() {
-    home +=1;
-    homeScore.textContent = home;    
-}  
-
-function addHome2() {
-    home +=2;
-    homeScore.textContent = home;    
-} 
-
-function addHome3() {
-    home +=3;
-    homeScore.textContent = home;    
+function addHomePoints(homePoints) {
+    home += homePoints
+    homeScore.textContent = home
 }
 
-function addguest1() {
-    guest +=1;
-    guestScore.textContent = guest;
-}
-
-function addguest2() {
-    guest +=2;
-    guestScore.textContent = guest;
-}
-function addguest3() {
-    guest +=3;
-    guestScore.textContent = guest;
+function addGuestPoints(guestPoints) {
+    guest += guestPoints
+    guestScore.textContent = guest
 }
 
 // Fouls Counters
@@ -46,16 +27,15 @@ let foulsGuest = document.getElementById("foul-guestEl");
 let addFoulHome= 0;
 let addFoulGuest = 0;
 
-function foulHome() {
-    addFoulHome +=1
+function foulHome(newHomeFoul) {
+    addFoulHome += newHomeFoul
     foulsHome.textContent = addFoulHome;
 }
 
-function foulGuest() {
-    addFoulGuest += 1
+function foulGuest(newGuestFoul) {
+    addFoulGuest += newGuestFoul
     foulsGuest.textContent= addFoulGuest;
 }
-
 
 // Reset Couters
 
